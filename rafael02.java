113a808c15a734cd148ed3ae7b2bc8e4a67edba5
rafaellkl11 <rafaelhenriqueif@gmail.com>
2024-09-24 11:20:36 -0300

    3°commit

---
+++ rafael02.java
@@ -0,0 +1,43 @@
+/**/
+
+import java.net.SocketTimeoutException;
+import java.util.*;
+public class rafael02 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        double [][] matriz = new double[3][5];
+        double[] vetor = new double [matriz.length];
+
+        System.out.println("2. Programa que recebe uma matriz e calcula o resultado da noma de cada linha");
+
+        for ( int i = 0 ; i < matriz.length ; i++){
+            for (int j = 0; j < matriz[i].length ; j++){
+                System.out.print("Digite o valor da linha "+(i+1)+" coluna "+(j+1)+": ");
+                matriz[i][j] = teclado.nextDouble();
+            }
+        }
+        
+        System.out.println("Matriz:");
+
+        for (int i = 0 ; i < matriz.length ; i++){
+            System.out.print(" | ");
+            for ( int j = 0 ; j < matriz[i].length ; j++){
+                System.out.print(matriz[i][j]+" | ");
+            }
+            System.out.print("\n");
+        }
+
+        for (int i = 0 ; i < matriz.length ;  i++){
+            for ( int j = 0 ; j < matriz[i].length ; j++){
+                vetor[i] = vetor[i] + matriz[i][j];
+            }
+        }
+
+        System.out.println("Resultado: ");
+        for (  int i = 0 ; i < vetor.length ; i++){
+            System.out.print(vetor[i]+", ");
+        }
+
+        teclado.close();
+    }
+}
