33596038e18a0d9b7d42d7856ea695ebc6789544
rafaellkl11 <rafaelhenriqueif@gmail.com>
2024-10-02 10:03:53 -0300

    11°commit

--- rafael06.java
+++ rafael06.java
@@ -9,10 +9,18 @@ seu gerente, mostrando ao mesmo a média e o desvio padrão de cada linha de pro
 exiba, se for o caso, qual das linhas de produção precisa ser revisada.*/
 
 import java.util.*;
+
+import javax.swing.text.AbstractDocument.LeafElement;
 public class rafael06 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        double [][] prod = new double[3][5];
+        double [][] prod = new double[6][5];
+        double [] media = new double [prod.length];
+        double [] variacaoVetor = new double [prod.length];
+        double variacaoSoma, soma;
+
+        soma = 0;
+        variacaoSoma =0;
 
         System.out.println("6. Programa que calcula o a variação de uma amostra em relação a média.");
 
@@ -20,6 +28,33 @@ public class rafael06 {
             for (int j = 0 ; j < prod[i].length ; j++){
                 System.out.print("Digite a quantidade da lata "+(j+1)+" linha "+(i+1)+": ");
                 prod[i][j] = teclado.nextDouble();
+            }
+        }
+
+        for (int i = 0 ; i < prod.length ; i++){
+            for (int j = 0 ; j < prod[i].length ; j++){
+                soma = soma + prod[i][j];
+            }
+            media[i] = soma / prod[i].length;
+            soma = 0;
+        }
+
+        for ( int i = 0 ; i < prod.length ; i++){
+            for ( int j = 0 ; j < prod[i].length; j++){
+                variacaoSoma = variacaoSoma + Math.pow((prod[i][j] - media[i]), 2);
+            }
+            variacaoVetor[i] = Math.sqrt(variacaoSoma / (prod[0].length - 1));
+            variacaoSoma = 0;
+        }
+
+        System.out.println("Resultado: ");
+
+        for (int i = 0 ; i < media.length ; i++){
+            if (media[i]+variacaoVetor[i] <= (390+5) && (media[i] - variacaoVetor[i]) >= (390-5)){
+                System.out.println("lote "+(i+1)+ " está dentro dos padrões");
+            }
+            else{
+                System.out.println("lote " +(i+1)+ " está  fora dos padrões");
             }
         }
 
